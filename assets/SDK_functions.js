f4926d7215ba4b172fa7c54057a2da73b06d69fd
Ben <benpersitz@airship.com>
2024-05-20 12:12:10 -0700

    updating forms to show one of each method(auto and manual)

--- assets/SDK_functions.js
+++ assets/SDK_functions.js
@@ -26,13 +26,13 @@ function prompt_registration() {
 function prompt_sms_form() {
 	let options = {
 		platform: "sms",
-    automatic: {
-      askAgainDelay: 0
-    },
+		// automatic: {
+		// 	askAgainDelay: 0
+		// },
 		i18n: {
 			en: {
 				terms: "These are terms that you agree to.",
-        footer: "This here be a footer",
+				footer: "This here be a footer",
 				placeholderMsisdn: "1360-867-5309",
 				submitButton: "Receive Texts",
 				invalidMsisdn: "Whoops, that number wasn't valid. Please try again"
@@ -44,24 +44,25 @@ function prompt_sms_form() {
 	UA.then((sdk) => {
 		return sdk.plugins.load(
 			"subscription-form",
-			"https://aswpsdkus.com/notify/v1/ua-subscription-form.min.js")
-    }).then((formFactory) => {
-		formFactory.setupModalForm(options);
-    // form.open()
+			"https://aswpsdkus.com/notify/v1/ua-subscription-form.min.js"
+		);
+	}).then((formFactory) => {
+		let form = formFactory.setupModalForm(options);
+		form.open()
 	});
 }
 
 function prompt_email_form() {
-  let options = {
+	let options = {
 		platform: "email",
-    size: "large",
-    automatic: {
-      askAgainDelay: 0
-    },
+		size: "large",
+		automatic: {
+			askAgainDelay: 0
+		},
 		i18n: {
 			en: {
 				terms: "These are terms that you agree to.",
-        footer: "This here be a footer",
+				footer: "This here be a footer",
 				placeholderEmail: "support@airship.com",
 				submitButton: "Receive Emails",
 				invalidEmail: "Whoops, that email wasn't valid. Please try again"
@@ -73,108 +74,77 @@ function prompt_email_form() {
 	UA.then((sdk) => {
 		return sdk.plugins.load(
 			"subscription-form",
-			"https://aswpsdkus.com/notify/v1/ua-subscription-form.min.js")
-    }).then((formFactory) => {
+			"https://aswpsdkus.com/notify/v1/ua-subscription-form.min.js"
+		);
+	}).then((formFactory) => {
 		formFactory.setupModalForm(options);
-    // form.open()
+		// form.open()  //call this if you aren't using automatic display
 	});
 }
 
 async function associateNamedUser() {
-  const namedUserId = document.querySelector('#nuid').value
-  const SDK = await UA
-  const channel = await SDK.getChannel()
-  channel.namedUser.set(namedUserId)
+	const namedUserId = document.querySelector("#nuid").value;
+	const SDK = await UA;
+	const channel = await SDK.getChannel();
+	channel.namedUser.set(namedUserId);
 }
 
-// async function addTags(e) {
-//   return true
-// 	const tagNU = document.getElementById("tagNU");
-//   const tagGroup = document.getElementById("tag-group").value
-//   const tagString = document.getElementById("tag-name").value
-//   const tagArray = tagString.split('x')
-// 	if (tagNU.checked) {
-//     const SDK = await UA
-//     const channel = await SDK.getChannel()
-//     channel.namedUser.tags.add(tagArray,tagGroup)
-// 	} else {
-//     channel.tags.add(tagArray,tagGroup)
-// 	}
-// }
-
-// function removeTags() {
-// 	const tagNU = document.getElementById("tagNU");
-
-// 	if (tagNU.checked) {
-// 		// Add SDK codes to remove tags on Named User
-// 	} else {
-// 		// Add SDK codes to remove tags on Channel
-// 	}
-// }
-
-// function setTags() {
-// 	const tagNU = document.getElementById("tagNU");
-
-// 	if (tagNU.checked) {
-// 		// Add SDK codes to set tags on Named User
-// 	} else {
-// 		// Add SDK codes to set tags on Channel
-// 	}
-// }
-async function changeTags(e){
-  const id = e.id
-  const tagNU = document.getElementById("tagNU");
-  const tagGroup = document.getElementById("tag-group").value
-  const tagString = document.getElementById("tag-name").value
-  console.log(tagGroup)
-  console.log(tagString)
-  // const tagArray = tagString.split('x')
-  // console.log(tagArray)
-  // console.log('hi')
-  const SDK = await UA
-  const channel = await SDK.getChannel()
-  console.log(channel)
-  if(tagNU.checked){
-      if(id == 'tag-add'){
-          channel.namedUser.tags.add(tagString,tagGroup)
-      } else if(id == 'tag-remove'){
-          channel.namedUser.tags.remove(tagString, tagGroup)
-      } else {
-          channel.namedUser.tags.set(tagString, tagGroup )
-      }
-  } else {
-      if(id == 'tag-add'){
-          channel.tags.add(tagString,tagGroup)
-      } else if(id == 'tag-remove'){
-          channel.tags.remove(tagString, tagGroup)
-          console.log('tag removed')
-      } else {
-          channel.tags.set(tagString, tagGroup )
-      }
-  }
+async function changeTags(e) {
+	const id = e.id;
+	const tagNU = document.getElementById("tagNU");
+	const tagGroup = document.getElementById("tag-group").value;
+	const tagString = document.getElementById("tag-name").value;
+	const SDK = await UA;
+	const channel = await SDK.getChannel();
+	if (tagNU.checked) {
+		if (id == "tag-add") {
+			channel.namedUser.tags.add(tagString, tagGroup);
+		} else if (id == "tag-remove") {
+			channel.namedUser.tags.remove(tagString, tagGroup);
+		} else {
+			channel.namedUser.tags.set(tagString, tagGroup);
+		}
+	} else {
+		if (id == "tag-add") {
+			channel.tags.add(tagString, tagGroup);
+		} else if (id == "tag-remove") {
+			channel.tags.remove(tagString, tagGroup);
+			console.log("tag removed");
+		} else {
+			channel.tags.set(tagString, tagGroup);
+		}
+	}
 }
 
 async function setAttrs() {
 	const attrNU = document.getElementById("attrNU");
-	const SDK = await UA
-	const channel = await SDK.getChannel()
-	const fnValue = document.querySelector("#first_name").value
-	const lnValue = document.querySelector("#last_name").value
-	const tierValue = document.querySelector("#loyalty_tier").value
-	const valueList = {"first_name": fnValue, "last_name": lnValue,"loyalty_tier": tierValue}
-	for(let value of Object.keys(valueList)){
-		if(valueList[value] == ''){
-			delete valueList[value]
-		}  else if (valueList[value] == "null"){
-			valueList[value] = ''
-			console.log("WOWWWW")
+	const SDK = await UA;
+	const channel = await SDK.getChannel();
+	const fnValue = document.querySelector("#first_name").value;
+	const lnValue = document.querySelector("#last_name").value;
+	const tierValue = document.querySelector("#loyalty_tier").value;
+	const valueList = {
+		first_name: fnValue,
+		last_name: lnValue,
+		loyalty_tier: tierValue
+	};
+	for (let value of Object.keys(valueList)) {
+		if (valueList[value] == "") {
+			delete valueList[value];
+		} else if (valueList[value] == "null") {
+			valueList[value] = "";
+			console.log("WOWWWW");
 		}
 	}
 	if (attrNU.checked) {
-		const results = await channel.namedUser.attributes.set(valueList)
-		results == true ? console.log("Attributes set") : console.log("An error occurred")
+		const results = await channel.namedUser.attributes.set(valueList);
+		results == true
+			? console.log("Attributes set")
+			: console.log("An error occurred");
 	} else {
-		const results = await channel.atributes.set(valueList)
-		console.log(results)
+		const results = await channel.atributes.set(valueList);
+		results == true
+			? console.log("Attributes set")
+			: console.log("An error occurred");
 	}
 }
